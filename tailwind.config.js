c19e5a7a0a293c6d9229f353fe14e95466d4733c
Babatunde-Ben <babatundeben110.com>
2022-05-13 00:57:51 +0100

    added svg assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
         primary: "#ffffff",
         secondary_1: "#17123B",
         secondary_2: "#333333",
+        secondary_3: "#444444",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat"],
       },
     },
   },
